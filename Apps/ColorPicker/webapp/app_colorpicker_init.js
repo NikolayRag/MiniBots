5f9ef62043d4663c7c4bb980208aa814743e1dc7
NikolayRag <thenickra@gmail.com>
2026-08-15 18:03:51 +0300

    change itent to tabs

--- Apps/ColorPicker/webapp/app_colorpicker_init.js
+++ Apps/ColorPicker/webapp/app_colorpicker_init.js
@@ -1,14 +1,14 @@
 const tg = window.Telegram?.WebApp;
 
 if (!tg) {
-  throw new Error('Telegram WebApp API is unavailable');
+	throw new Error('Telegram WebApp API is unavailable');
 }
 
 tg.ready();
 
 if (typeof tg.requestFullscreen === 'function') {
-  try {
-    tg.requestFullscreen();
-  } catch(e){testcontent?.innerHTML=`${e}`;}
+	try {
+		tg.requestFullscreen();
+	} catch(e) catch(e){testcontent?.innerHTML=`${e}`;}
 }
 
