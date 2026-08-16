eb62896a24b7c8935b56b6ff4062e6abc8aaea3b
NikolayRag <thenickra@gmail.com>
2026-08-16 03:05:20 +0300

    fs fix att

--- Apps/ColorPicker/webapp/app_colorpicker_init.js
+++ Apps/ColorPicker/webapp/app_colorpicker_init.js
@@ -9,16 +9,11 @@ if (!tg) {
 tg.ready();
 
 
-if (typeof tg.disableVerticalSwipes === 'function') {
-	try {
-		tg.disableVerticalSwipes();
-	} catch(e) testcontent.innerHTML += `<br><br>${e}`;;
-}
 
 
 if (typeof tg.requestFullscreen === 'function') {
 	try {
 		tg.requestFullscreen();
-	} catch(e) testcontent.innerHTML += `<br><br>${e}`;;
+	} catch(e) testcontent.innerHTML += `<br><br>${e}`;
 }
 
