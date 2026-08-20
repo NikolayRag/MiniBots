f10b61b3610bc3b914b05f55a0fbfc1ddb07f77c
NikolayRag <thenickra@gmail.com>
2026-08-20 09:16:39 +0300

    clean

--- Apps/ColorPicker/webapp/app_frame.js
+++ Apps/ColorPicker/webapp/app_frame.js
@@ -10,39 +10,34 @@ function errlog(e){
 }
 
 
-var inDataA = new URLSearchParams(location.search);
-alert(inDataA);
+
 
 
 
 const tg = window.Telegram?.WebApp;
-
 if (!tg)
 	throw new Error('Telegram WebApp API is unavailable');
 
 tg.ready();
 
 
-
-if (typeof tg.disableVerticalSwipes === 'function') {
+if (typeof tg.disableVerticalSwipes === 'function')
 	try {
 		tg.disableVerticalSwipes();
 		errlog(`<br><br>disableVerticalSwipes`);
 	} catch(e) {
 		errlog(`${e}`);
 	}
-}
 
 
 
-if (typeof tg.requestFullscreen === 'function') {
+if (typeof tg.requestFullscreen === 'function')
 	try {
 		tg.requestFullscreen();
 		errlog(`<br><br>requestFullscreen`);
 	} catch(e) {
 		errlog(`${e}`);
 	}
-}
 
 
 
