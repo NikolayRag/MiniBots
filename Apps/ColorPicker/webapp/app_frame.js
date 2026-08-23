cd06d9b35e5287b5c2f3c12c0a8552fb41239677
NikolayRag <thenickra@gmail.com>
2026-08-24 02:53:57 +0300

    try

--- Apps/ColorPicker/webapp/app_frame.js
+++ Apps/ColorPicker/webapp/app_frame.js
@@ -9,11 +9,15 @@ requestFullscreen
 //// +FUNCTIONS
 
 async function fetchUserProfile(_initData, _botName){
+	errlog(`${_botName} initData: ${decodeURIComponent(JSON.stringify(tg.initData))}`);
+
+
 	let response;
 	try{
 		response = await fetch('https://minibot-authgate.nikolayr.workers.dev/', {
 			headers: {
-				'X-Telegram-Init-Data': _initData
+				'X-Telegram-Init-Data': _initData,
+				'bound-to-bot': _botName
 			}
 		});
 	} catch(e){
@@ -59,7 +63,6 @@ if (typeof tg.requestFullscreen === 'function')
 	}
 
 
-errlog(`initData: ${decodeURIComponent(JSON.stringify(tg.initData))}`);
 fetchUserProfile(tg.initData, botBound);
 
 //Telegram.WebApp.sendData([rgb0.r, rgb1.r, rgb0.g, rgb1.g, rgb0.b, rgb1.b].join(',')); 
