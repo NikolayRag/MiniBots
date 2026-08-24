d2505dad242beec72b2dce21de100e6a94860e28
NikolayRag <thenickra@gmail.com>
2026-08-24 03:12:56 +0300

    Update app_frame.js

--- Apps/ColorPicker/webapp/app_frame.js
+++ Apps/ColorPicker/webapp/app_frame.js
@@ -62,9 +62,6 @@ if (typeof tg.requestFullscreen === 'function')
 	}
 
 
-//fetchUserProfile(tg.initData, botBound);
-
-const ii = 'query_id=AAG9Xk4VAAAAAL1eThVIvCkC&user={"id":357457597,"first_name":"Nikolaу","last_name":"Ragozin","username":"ki_privet","language_code":"en","allows_write_to_pm":true,"photo_url":"https:\/\/t.me\/i\/userpic\/320\/ILa5pGzjRz1i8JntV2VFW9fnS0CmE7VyTRWdBlZWDUE.svg"}&auth_date=1787530134&signature=fKWIMzkX-4A3tZl6Zau7P8wL3vBGwpD3SQ-dKjh0Q0M7Z19hd1ShYS6zkRPz3eoonNmD6tyynrR-6OCbSFY5Aw&hash=1e9eb971b74a25169c3761256a1bc56b67135045aa8483afa8c575631551ff6f';
-fetchUserProfile(ii, botBound);
+fetchUserProfile(tg.initData, botBound);
 
 //Telegram.WebApp.sendData([rgb0.r, rgb1.r, rgb0.g, rgb1.g, rgb0.b, rgb1.b].join(',')); 
