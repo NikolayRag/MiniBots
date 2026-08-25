032993d2b3454dba6e7718cd51838e0791a1d194
NikolayRag <thenickra@gmail.com>
2026-08-25 03:18:50 +0300

    update app_frame

--- Shared/app_frame/app_frame.js
+++ Shared/app_frame/app_frame.js
@@ -5,52 +5,62 @@ requestFullscreen
 */
 
 
-const tg = window.Telegram?.WebApp;
 
-if (!tg) {
-  throw new Error('Telegram WebApp API is unavailable');
+
+async function procceedAuthenticate(_initData, _botName){
+	errlog(`"${_botName}" initData: ${decodeURIComponent(JSON.stringify(tg.initData))}`);
+
+	let response;
+	try{
+		response = await fetch('https://minibot-authgate.nikolayr.workers.dev/', {
+			headers: {
+				'X-Telegram-Init-Data': _initData,
+				'bound-to-bot': _botName
+			}
+		});
+	} catch(e){
+		errlog(`Fetch Error: ${e}`);
+	}
+
+	response && errlog(`Fetch: ${response.status}; ${await response.text()}`);
 }
+
+function errlog(e){
+	if (typeof logContainer != 'undefined')
+		logContainer.innerHTML += `<br><br>${e}`;
+}
+
+
+
+//// -FUNCTIONS
+
+
+
+const tg = window.Telegram?.WebApp;
+if (!tg)
+	throw new Error('Telegram WebApp API is unavailable');
 
 tg.ready();
 
 
-function errlog(e){
-  if (typeof logContainer != 'undefined')
-    logContainer.innerHTML += e;
-}
-
-
-if (typeof tg.disableVerticalSwipes === 'function') {
-  try {
-    tg.disableVerticalSwipes();
-    errlog(`<br><br>disableVerticalSwipes`);
-  } catch(e) {
-    errlog(`${e}`);
-  }
-}
+if (typeof tg.disableVerticalSwipes === 'function')
+	try {
+		tg.disableVerticalSwipes();
+		errlog(`disableVerticalSwipes`);
+	} catch(e) {
+		errlog(`${e}`);
+	}
 
 
 
-if (typeof tg.requestFullscreen === 'function') {
-  try {
-    tg.requestFullscreen();
-    errlog(`<br><br>requestFullscreen`);
-  } catch(e) {
-    errlog(`${e}`);
-  }
-}
+if (typeof tg.requestFullscreen === 'function')
+	try {
+		tg.requestFullscreen();
+		errlog(`requestFullscreen`);
+	} catch(e) {
+		errlog(`${e}`);
+	}
 
 
-/*
-function fetchUserProfile(){
-  const response = await fetch('/api/context', {
-    headers: {
-      'X-Telegram-Init-Data': tg.initData
-    }
-  });
+procceedAuthenticate(tg.initData, botBound);
 
-  const context = await response.json();
-
-  errlog(`<br><br>${context}`);
-}
-*/
